109596c001d45cc048d0da3fda0488d8d75fa2f0
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-07-28 21:25:49 +0800

    add options for emojione

--- src/filter.js
+++ src/filter.js
@@ -1,7 +1,12 @@
 import emojione from 'emojione'
 
 export default {
-  install (Vue) {
+  install (Vue, options = {
+    imageType: 'svg',
+    sprites: true
+  }) {
+    Object.assign(emojione, options)
+
     Vue.filter('emoji', (value, method = 'toImage') => {
       return emojione[method](value)
     })
